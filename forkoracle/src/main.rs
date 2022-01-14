824827e9890a622337117b41d0e56538e0cf459c
Hammad Tariq <hammadtariq@yahoo.co.uk>
2022-01-14 19:06:59 +0500

    added test functions

--- forkoracle/src/main.rs
+++ forkoracle/src/main.rs
@@ -1,7 +1,9 @@
-use forkscanner::{ForkScanner, run_server};
+use forkscanner::{ForkScanner, Block, run_server};
 use log::info;
 use diesel::prelude::PgConnection;
 use diesel::Connection;
+use diesel::prelude::*;
+use diesel::result::QueryResult;
 
 fn main() {
     dotenv::dotenv().expect("Failed loading dotenv");
@@ -13,7 +15,7 @@ fn main() {
 
     println!("Forkscanner is running...");
 
-    let _handle = std::thread::spawn(move || {
+    let _handle_scanner = std::thread::spawn(move || {
         loop {
             scanner.run();
             info!("Run finished, sleeping");
@@ -21,5 +23,33 @@ fn main() {
         }
     });
 
+    let _handle_kdeep_blocks = std::thread::spawn(move || {
+        loop {
+            get_kdeep_blocks();
+            info!("Fetched k_deep blocks, sleeping");
+            std::thread::sleep(duration);
+        }
+    });
+
     run_server("127.0.0.1:8339", &db_url);
+}
+
+pub fn get_kdeep_blocks_query(conn: &PgConnection) -> QueryResult<Vec<Block>> {
+    let raw_query = format!("
+        WITH RECURSIVE rec_query AS (
+            SELECT * FROM blocks WHERE hash = (select block from chaintips where status = 'active' )
+            UNION ALL
+            SELECT b.* FROM blocks b INNER JOIN rec_query r ON r.parent_hash = b.hash
+        ) SELECT * FROM rec_query ORDER BY height DESC Limit 10;
+    ");
+
+    diesel::sql_query(raw_query).load(conn)
+}
+
+fn get_kdeep_blocks() {
+    dotenv::dotenv().expect("Failed loading dotenv");
+    let db_url = std::env::var("DATABASE_URL").expect("No DB url");
+    let db_conn = PgConnection::establish(&db_url).expect("Connection failed");
+    let blocks = get_kdeep_blocks_query(&db_conn).unwrap();
+    println!("{:?}", blocks[0].hash);
 }
